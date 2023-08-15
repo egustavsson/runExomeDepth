36d11bf6df9cac366125d3e495e3b235f9d3063e
egustavsson <e.gustavsson@ucl.ac.uk>
2023-08-15 15:55:49 +0100

    added check to install-packages.R

--- install-packages.R
+++ install-packages.R
@@ -1,5 +1,7 @@
-# Rrequired for Bioconductor packages
-install.packages("BiocManager", repos = "https://cran.ma.imperial.ac.uk/")
+# Required for Bioconductor packages
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
+  install.packages("BiocManager", repos = "https://cran.ma.imperial.ac.uk/")
+}
 
 # Bioconductor packages
 bioc_packages <- c("Biostrings", "IRanges", "Rsamtools", "GenomicRanges", "GenomicAlignments", "rtracklayer")
@@ -16,7 +18,7 @@ for (package in bioc_packages) {
   install_bioc_if_not_installed(package)
 }
 
-# Cunction to install a CRAN package if it is not already installed
+# Function to install a CRAN package if it is not already installed
 install_cran_if_not_installed <- function(package) {
   if (!requireNamespace(package, quietly = TRUE)) {
     install.packages(package, repos = "https://cran.ma.imperial.ac.uk/")
@@ -29,5 +31,12 @@ for (package in cran_packages) {
   install_cran_if_not_installed(package)
 }
 
-
-
+# Check and print installed packages
+installed_packages <- c(bioc_packages, cran_packages)
+for (package in installed_packages) {
+  if (requireNamespace(package, quietly = TRUE)) {
+    cat("Package", package, "is installed.\n")
+  } else {
+    cat("Package", package, "is not installed.\n")
+  }
+}
