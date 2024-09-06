d9106ea5e451732282b9054208f55c416e39da54
egustavsson <e.gustavsson@ucl.ac.uk>
2024-09-06 12:06:04 +0100

    added print statements

--- ExomeDepth.R
+++ ExomeDepth.R
@@ -173,6 +173,8 @@ base_Counts <- getBamCounts(bed.frame = targets,
                             bam.files = baseline_samples$baseline_sample_path,
                             include.chr = TRUE) %>%
   setNames(gsub("^X(\\d+)", "\\1", names(.)))
+print("Done parsing baseline samples!")
+print("Starting to process test samples!")
 
 # Run the analysis for each test sample
 for (test_sample_path in test_samples$test_sample_path) {
@@ -210,3 +212,4 @@ for (test_sample_path in test_samples$test_sample_path) {
     sink()  # Stop redirecting stdout
   })
 }
+print("Analysis completed!")
